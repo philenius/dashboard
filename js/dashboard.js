a41a68cdd79c5be16b5103af003b3b53a44321b1
Philipp <philippus-perez@web.de>
2015-03-29 23:40:18 +0200

    added slide-menu for leaving dashboard

--- js/dashboard.js
+++ js/dashboard.js
@@ -165,3 +165,22 @@ $('button').click(function () {
 $(function () {
    $('[data-toggle="tooltip"]').tooltip();
 });
+
+// slide-menu
+var $open = false;
+$('#slide-button').click(function() {
+    if (!$open) {
+        $('.slide-menu').animate({left: '20px', borderTopLeftRadius: '0px', borderBottomLeftRadius: '0px'}, 1000);
+    } else {
+        $('.slide-menu').animate({left: '230px', borderTopLeftRadius: '20px', borderBottomLeftRadius: '20px'}, 1000);
+    }
+    $open = !$open;
+});
+$('#slide-menu').mouseenter(function() {
+    if(!$open)
+        $('#slide-menu').animate({left: '220px'}, 200);
+});
+$('#slide-menu').mouseleave(function() {
+    if (!$open)
+        $('#slide-menu').animate({left: '230px'}, 200);
+});
